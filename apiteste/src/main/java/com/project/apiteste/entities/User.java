b8a88be0e866651082d80012d734c87b91aee052
Bruno Souza <brunocloud78@gmail.com>
2024-09-12 17:10:02 -0300

    Repository, dependency

--- apiteste/src/main/java/com/project/apiteste/entities/User.java
+++ apiteste/src/main/java/com/project/apiteste/entities/User.java
@@ -3,9 +3,21 @@ package com.project.apiteste.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -16,7 +28,7 @@ public class User implements Serializable {
 		
 	}
 
-	public User(long l, String name, String email, String phone, String password) {
+	public User(Long l, String name, String email, String phone, String password) {
 		super();
 		this.id = l;
 		this.name = name;
---
+++ apiteste/src/main/java/com/project/apiteste/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.project.apiteste.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.apiteste.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+
+
+
+
+}
